7492c8a3965d845559de143e10683b49ad43feaf
Armensch11 <armenschk@gmail.com>
2023-05-20 20:40:38 +0400

    loggedIn user homepage done

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,24 +1,52 @@
-import { Button, TextField, Typography } from "@mui/material";
+import { Button, IconButton, TextField, Typography } from "@mui/material";
 import "./Home.css";
-import { useAppSelector } from "../../hooks/typedReduxHooks/typedReduxHooks";
+import {
+  useAppDispatch,
+  useAppSelector,
+} from "../../hooks/typedReduxHooks/typedReduxHooks";
+import { logOut } from "../../reducers/authSlice";
+import filterIcon from "../../assets/filter-Icon.svg";
+
 const Home = () => {
   const userEmail = useAppSelector((state) => state.authState.userEmail);
+  const dispatch = useAppDispatch();
+
+  const onLogout = () => {
+    dispatch(logOut());
+    localStorage.clear();
+  };
+
   return (
     <>
       <div className="home-container"></div>
       <div className="homePage-header">
         <Typography>{userEmail}</Typography>
-        <Button>{"Logout"}</Button>
+        <Button onClick={onLogout}>{"Logout"}</Button>
       </div>
       <hr className="blue-line" />
       <div className="homePage-search">
         <TextField
           type={"search"}
-          sx={{ width: "80%" }}
+          sx={{ width: "80%", minWidth: "500px" }}
           InputProps={{ sx: { height: 40 } }}
         ></TextField>
-        <Button>{"Search"}</Button>
-        <Button>{"filterIcon"}</Button>
+        <Button
+          sx={{
+            width: "180px",
+            borderRadius: "8px",
+            backgroundColor: "rgba(60, 134, 244, 0.2)",
+          }}
+        >
+          {"Search"}
+        </Button>
+        <IconButton
+          sx={{
+            borderRadius: "8px",
+            backgroundColor: "rgba(60, 134, 244, 0.2)",
+          }}
+        >
+          <img src={filterIcon} alt="filter icon" />
+        </IconButton>
       </div>
       <main className="homePage-main"></main>
     </>
