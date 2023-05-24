b66704c6428754b65b1ca5108a307c65d214c8fa
Armensch11 <armenschk@gmail.com>
2023-05-24 11:46:37 +0400

    publications some styling

--- src/components/protein/publications/publicItem/PublicItem.tsx
+++ src/components/protein/publications/publicItem/PublicItem.tsx
@@ -1,5 +1,6 @@
 import { Button, Typography } from "@mui/material";
 import { FC } from "react";
+import "./PublicItem.css";
 
 type Link = {
   database: string;
@@ -26,11 +27,22 @@ const PublicItem: FC<PublicItemProps> = ({
   return (
     <>
       <div className="publication-container">
-        <Typography>{title}</Typography>
-        <Typography>{authors?.join(",")}</Typography>
-        <Typography>{`Categories: ${categories?.join(", ")}`}</Typography>
-        <Typography>{`Citied for: ${citied?.join(", ")}`}</Typography>
-        <Typography>{`Source: ${source}`}</Typography>
+        <Typography
+          variant="subtitle1"
+          sx={{ fontWeight: "bold", color: "#000000" }}
+        >
+          {title}
+        </Typography>
+        <Typography variant="body2" mb={2}>
+          {authors?.join(",")}
+        </Typography>
+        <Typography variant="body2">{`Categories: ${categories?.join(
+          ", "
+        )}`}</Typography>
+        <Typography variant="body2">{`Citied for: ${citied?.join(
+          ", "
+        )}`}</Typography>
+        <Typography variant="body2">{`Source: ${source}`}</Typography>
         <div className="links-container">
           <Button></Button>
           <Button></Button>
